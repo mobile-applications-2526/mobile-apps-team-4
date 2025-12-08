b6a3fdee971d71cadf6972c22e10bb667fcaf782
Arnovda1 <arno@van-der-auwera.com>
2025-12-08 14:20:04 +0100

    frontend removed try catch in services

--- frontend/src/services/GroupService.ts
+++ frontend/src/services/GroupService.ts
@@ -2,43 +2,27 @@ import { Group } from "@/types";
 import api from "./api";
 
 const getAll = async (): Promise<Group[] | undefined> => {
-  try {
-    const res = await api.get<Group[]>('/groups/all');
-    return res.data;
-  } catch (err) {
-    throw err;
-  }
+  const res = await api.get<Group[]>('/groups/all');
+  return res.data;
 };
 
 const get = async (id: number): Promise<Group | undefined> => {
-  try {
-    const res = await api.get<Group>(`/groups/${id}`);
-    return res.data;
-  } catch (err) {
-    throw err;
-  }
+  const res = await api.get<Group>(`/groups/${id}`);
+  return res.data;
 };
 
 const getJoined = async (): Promise<Group[] | undefined> => {
-  try {
-    const res = await api.get<Group[]>('/groups/joined');
-    return res.data;
-  } catch (err) {
-    throw err;
-  }
+  const res = await api.get<Group[]>('/groups/joined');
+  return res.data;
 };
 
 const create = async ({ name, members, description}: { name: string, members?: number[], description?: string }): Promise<Group> => {
-  try {
-    const res = await api.post<Group>(`/groups/create?name=${name}`, {
-      name,
-      description,
-      members,
-    });
-    return res.data;
-  } catch (err) {
-    throw err;
-  }
+  const res = await api.post<Group>(`/groups/create?name=${name}`, {
+    name,
+    description,
+    members,
+  });
+  return res.data;
 };
 
 const GroupService = {
