70f70091d37cff5b3882efbee3d8507889f06942
Arnovda1 <arno@van-der-auwera.com>
2025-12-08 16:06:37 +0100

    frontend invite user works

--- frontend/app/(modals)/inviteUser.tsx
+++ frontend/app/(modals)/inviteUser.tsx
@@ -9,9 +9,9 @@ import { useAuth } from "@/context/AuthContext";
 import { Colors } from "@/constants/theme";
 import { IconSymbol } from "@/components/ui/icon-symbol";
 import { router, useLocalSearchParams } from "expo-router";
-import Toast from "react-native-toast-message";
 import GroupService from "@/services/GroupService";
 import showErrorToast from "@/utils/showErrorToast";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const InviteUser = () => {
   const styles = useGlobalStyles();
@@ -21,6 +21,20 @@ const InviteUser = () => {
 
   const [search, setSearch] = useState<string>('');
   const [foundUsers, setFoundUsers] = useState<User[]>([]);
+  const [invitedUsers, setInvitedUsers] = useState<User[]>([]);
+  const [joinedUsers, setJoinedUsers] = useState<User[]>([]);
+
+  const updateUsers = async () => {
+    if (!groupId) return;
+
+    const res = await GroupService.get(Number(groupId));
+    setInvitedUsers(res?.invitedMembers || []);
+    setJoinedUsers(res?.members || []);
+  };
+
+  useEffect(() => {
+    updateUsers();
+  }, []);
 
   useEffect(() => {
     const getUsers = async () => {
@@ -29,7 +43,14 @@ const InviteUser = () => {
       } else {
         const res = await UserService.findByEmailOrName(search);
 
-        setFoundUsers(res.filter(u => u.id !== user?.id)); // exclude logged in user
+        const alreadyInvitedIds = new Set(invitedUsers.map(u => u.id));
+        const joinedUserIds = new Set(joinedUsers.map(u => u.id));
+
+        setFoundUsers(res
+          .filter(u => u.id !== user?.id) // exclude logged in user (group owner)
+          .filter(u => !alreadyInvitedIds.has(u.id)) // exclude already invited
+          .filter(u => !joinedUserIds.has(u.id)) // exclude joined members
+        );
       }
 
     };
@@ -43,52 +64,78 @@ const InviteUser = () => {
   }
 
   const inviteUser = async (user: User) => {
-    
     try {
       await GroupService.inviteUser({ groupId: Number(groupId), userId: user.id });
 
-      Toast.show({
-        type: 'success',
-        text1: `Invited ${user.name}`,
-      });
+      updateUsers();
+      setSearch('');
     } catch (err) {
       showErrorToast(err);
     }
-    
-    router.back();
   };
 
+  const cancelInvite = async (user: User) => {
+    try {
+      await GroupService.cancelInvite({ groupId: Number(groupId), userId: user.id });
+
+      updateUsers();
+    } catch (err) {
+      showErrorToast(err);
+    }
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ModalHeading name="Invite a member" />
+    <SafeAreaView style={styles.containerNoPadding}>
+      <KeyboardAwareScrollView style={styles.container}>
+        <ModalHeading name="Invite a member" />
 
-      <Text style={styles.label}>Search user</Text>
-      <TextInput
-        value={search}
-        onChangeText={setSearch}
-        placeholder='Name'
-        placeholderTextColor={styles.placeholderText.color}
-        returnKeyType="done"
-        style={styles.input}
-      />
+        <Text style={styles.label}>Search user</Text>
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder='Name'
+          placeholderTextColor={styles.placeholderText.color}
+          returnKeyType="done"
+          style={styles.input}
+        />
 
-      <FlatList
-        data={foundUsers}
-        keyExtractor={item => item.id.toString()}
-        scrollEnabled={false}
-        style={{ marginBottom: 8, ...(foundUsers.length > 0 && { ...styles.borderColor, borderTopWidth: 1 }) }}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-          style={styles.listItemSmall}
-          onPress={() => inviteUser(item)}
-          >
-            <IconSymbol size={20} name="plus" color={isDark ? Colors.dark.tint : Colors.light.tint} />
-            <Text style={{ fontSize: 16, fontWeight: 'bold', ...styles.text }}>{item.name}</Text>
-            <Text style={{ fontSize: 14, marginLeft: 'auto', ...styles.text }}>{item.email}</Text>
-          </TouchableOpacity>
-        )}
-      />
+        <FlatList
+          data={foundUsers}
+          keyExtractor={item => item.id.toString()}
+          scrollEnabled={false}
+          style={{ marginBottom: 8, ...(foundUsers.length > 0 && { ...styles.borderColor, borderTopWidth: 1 }) }}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.listItemSmall}
+              onPress={() => inviteUser(item)}
+            >
+              <IconSymbol size={20} name="plus" color={isDark ? Colors.dark.tint : Colors.light.tint} />
+              <Text style={{ fontSize: 16, fontWeight: 'bold', ...styles.text }}>{item.name}</Text>
+              <Text style={{ fontSize: 14, marginLeft: 'auto', ...styles.text }}>{item.email}</Text>
+            </TouchableOpacity>
+          )}
+        />
 
+        <Text style={styles.label}>Invited users</Text>
+        <FlatList
+          data={invitedUsers}
+          keyExtractor={item => item.id.toString()}
+          scrollEnabled={false}
+          style={{ marginBottom: 8, ...(invitedUsers.length > 0 && { ...styles.borderColor, borderTopWidth: 1 }) }}
+          ListEmptyComponent={<Text style={styles.text}>No pending invites</Text>}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.listItemSmall}
+              onPress={() => cancelInvite(item)}
+            >
+              <IconSymbol size={20} name="xmark" color={isDark ? Colors.dark.tint : Colors.light.tint} />
+              <Text style={{ fontSize: 16, fontWeight: 'bold', ...styles.text }}>{item.name}</Text>
+              <Text style={{ fontSize: 14, marginLeft: 'auto', ...styles.text }}>{item.email}</Text>
+            </TouchableOpacity>
+          )}
+        />
+
+      </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 };
